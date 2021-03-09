b851f315e60d48ae4752ed2d744312206dc60845
Hidde Ewout Neuteboom <Hidde.neuteboom@gmail.com>
2021-03-09 10:19:40 +0100

    Discoball WIP

---
+++ Assets/HiddeMap/HiddeCode/DiscoBall.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoBall : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] float changeTime = 10f;
+    [SerializeField] Material[] materials;
+    [SerializeField] MeshRenderer mesh;
+    [SerializeField] float rotationSpeed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = gameObject.GetComponent<MeshRenderer>();
+        StartCoroutine(ColourChange());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+    }
+
+    IEnumerator ColourChange()
+    {
+        while (true)
+        {
+            
+            int materialUsed;
+            materialUsed = Random.Range(0, materials.Length);
+
+            mesh.material = materials[materialUsed];
+
+            yield return new WaitForSeconds(changeTime);
+        }
+
+    }
+
+}
